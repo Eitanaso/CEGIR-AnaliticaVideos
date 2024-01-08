77b873e6c56d0cc2c0e27e598fb6d74c3797ff5d
Eitanaso <53804725+Eitanaso@users.noreply.github.com>
2024-01-08 14:17:18 -0300

    Add files via upload

--- detectar.py
+++ detectar.py
@@ -6,9 +6,17 @@ from funciones.general import *
 from funciones.estacionados import *
 from clases.objeto_global import Objeto_Global
 
+#import queue
+#import threading
+
+import pandas as pd
+import datetime as datetime
+
+
+
 #--------------------------------------------------------------------------------------------------------------------
 # Creacion de funciones para iniciar el procesamiento segun lo solicitado
-# Funciones creadas a partir de código de la libreria Supervision y de codigo para la conexion a camaras de Luis Escares
+# Funciones creadas a partir de código de la libreria Supervision y de codigo para la conexion a camaras y guardado de archivos de Luis Escares
 # Por mayores dudas de las funciones, comunicarse con Eitan Hasson Arellano a traves del correo eitanhass@gmail.com
 #--------------------------------------------------------------------------------------------------------------------
 
@@ -34,13 +42,33 @@ def detect(frame, model, selected_classes, tareas):
             annotated_frame = tareas.objeto_Estacionados.anotar_frame(annotated_frame, detections, model)
         if tareas.contadores:
             annotated_frame = tareas.objeto_Contadores.anotar_frame(annotated_frame, detections, model)
+        if tareas.trayectorias:
+            annotated_frame = tareas.objeto_Trayectorias.anotar_frame(annotated_frame, detections, model)
+        if tareas.velocidades:
+            if not tareas.velocidad_por_zonas:
+                annotated_frame = tareas.objeto_Velocidades.anotar_frame(annotated_frame, detections, model)
+            else:
+                annotated_frame = tareas.objeto_Velocidades.anotar_frame_zonas(annotated_frame, detections, model)
 
     return  annotated_frame
 
+
+
 def run_detect(cap, model, clases, fps, procesamiento, tiempos=[], i=0):
 
-    ret, frame = cap.read()
+    #p1 = threading.Thread(target=Receive(cap))
+    #p2 = threading.Thread(target=Display(procesamiento, fps, model, clases))
+    #p1.start()
+    #p2.start()
 
+    ret, frame = cap.read()
+    segundo_entre_registros = 1
+    segundos_reinicio_contador = 3
+    columnas_indicador = {'id_camara':[], 'calles_camara':[], 'fecha':[], 'hora_inicio':[], 'hora_final':[], 'flujo_personas_oeste_este':[], 'flujo_personas_este_oeste':[]}
+    df=pd.DataFrame(columnas_indicador)
+    momento = datetime.datetime.now()
+    momento = f'{momento.month}_{momento.day}_{momento.hour}_{momento.minute}'
+    
     if not procesamiento['solo_mostrar']:
         tareas = Objeto_Global(fps)
         tareas.create_byte_tracker()
@@ -49,7 +77,11 @@ def run_detect(cap, model, clases, fps, procesamiento, tiempos=[], i=0):
         if procesamiento['mostrar_estacionados']:
             tareas.create_estacionados(frame, procesamiento['zonas_estacionados'], procesamiento['centros_zonas_estacionados'])
         if procesamiento['mostrar_contadores']:
-            tareas.create_contadores(procesamiento['lineas_contadores'])
+            tareas.create_contadores(procesamiento['lineas_contadores'])#, procesamiento['guardar_archivo_contador'])
+        if procesamiento['mostrar_trayectorias']:
+            tareas.create_trayectorias(frame, procesamiento['guardar_videos_evento'])
+        if procesamiento['mostrar_velocidades']:
+            tareas.create_velocidades(frame, procesamiento['velocidades_por_zonas'], procesamiento['zonas_velocidades'], procesamiento['min_max_zonas'], procesamiento['guardar_videos_evento'])
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -62,6 +94,13 @@ def run_detect(cap, model, clases, fps, procesamiento, tiempos=[], i=0):
             output_frame = frame
         else:
             output_frame = detect(frame, model, clases, tareas)
+            if procesamiento['guardar_archivo_contador']:
+                if i % int(fps * segundo_entre_registros) == 0:
+                    df = guardar_xlsx_contador(df, tareas.objeto_Contadores.contadores[0])
+                if i % int(fps * segundos_reinicio_contador) == 0:
+                    df.to_csv(r'C:\Users\eitan\Pictures\comercio_ambulante_paseo_estacion_indicadores_transito_peatonal_cruz_verde.csv', index=False)
+                    #df.to_csv(r'C:\Users\admin\Desktop\archivo_generado\paseo_estacion_indicadores_transito_peatonal_cruz_verde' + momento + '.csv', index=False)
+                    tareas.objeto_Contadores.contadores[0].reset()
         tiempos.append((time.time() - ti)*1000)
         i += 1
         #output_frame=frame
@@ -69,4 +108,19 @@ def run_detect(cap, model, clases, fps, procesamiento, tiempos=[], i=0):
         # Display the frame with detections
         cv2.imshow('Camara Paseo Estacion con analitica', output_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            df.to_csv(r'C:\Users\eitan\Pictures\comercio_ambulante_paseo_estacion_indicadores_transito_peatonal_cruz_verde.csv', index=False)
+            #df.to_csv(r'C:\Users\admin\Desktop\archivo_generado\paseo_estacion_indicadores_transito_peatonal_cruz_verde' + momento + '.csv', index=False)
             break
+
+def guardar_xlsx_contador(df, contador):
+    id_camara = '1'
+    calles_camara = 'paseo_estacion_cruz_verde'
+    fecha_hora = datetime.datetime.now()
+    fecha= f'{fecha_hora.year}_{fecha_hora.month}_{fecha_hora.day}'
+    hora_inicio= f'{fecha_hora.hour}_{fecha_hora.minute}_{fecha_hora.second}'
+    hora_final = '-'
+    flujo_personas_oeste_este = contador.in_count
+    flujo_personas_este_oeste = contador.out_count
+    nueva_fila = {'id_camara':id_camara, 'calles_camara':calles_camara, 'fecha':fecha, 'hora_inicio':hora_inicio, 'hora_final':hora_final, 'flujo_personas_oeste_este':flujo_personas_oeste_este, 'flujo_personas_este_oeste':flujo_personas_este_oeste}
+    df = pd.concat([df, pd.DataFrame(nueva_fila, index=[0])], ignore_index=True)
+    return df
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import argparse
-from detectar import run_detect
+from detectar import run_detect, detect
 from general import get_video, get_model
+from clases.objeto_global import Objeto_Global
+
+import queue
+import threading
+import multiprocessing
+import time
+import cv2
+
+q = queue.Queue()
 
 #--------------------------------------------------------------------------------------------------------------------
 # Creacion de variables y funcion main para el control de los distintos tipos de analisis a realizar en videos
@@ -20,34 +29,113 @@ parser = argparse.ArgumentParser(description='Ejecutar la deteccion del modelo')
 
 
 SOURCE_VIDEO_PATH = r'C:\Users\eitan\Pictures\comercio_ambulante_paseo_estacion.mp4'
+#SOURCE_VIDEO_PATH = r'rtsp://root:pass@10.0.10.184/axis-media/media.amp'
+#SOURCE_VIDEO_PATH = r'rtsp://admin:Cafa2414$@10.0.10.182:554/3/profile2/media.smp'
 #TARGET_VIDEO_PATH = r'C:\Users\eitan\Pictures\comercio_ambulante_detectado.mp4'
 
 rtsp_url = SOURCE_VIDEO_PATH
 MODEL = "yolov8n.pt"
-selected_classes = [0,2]
+selected_classes = [0]
 fps = 25
 
 procesamiento = {
     'solo_mostrar': False,
     'solo_detector': False,
-    'mostrar_contadores': True,
+    'mostrar_contadores': False,
     'lineas_contadores': [
         #[(125, 333), (483, 195)],
-        [(0, 430), (160, 334)],
+        
+        #[(450, 211), (1048, 207)],
+        #[(1462, 768), (1739,775)],
+        #[(334, 648), (1115, 350)],
+
+        #[(0, 430), (160, 334)],
+        [(160, 334), (468, 185)],
     ],
+    'guardar_archivo_contador': False, 
     'mostrar_estacionados': False,
     'zonas_estacionados': [
         [(40, 188), (187, 337), (371, 412), (408, 333), (265, 273), (131, 167)],
         [(427, 256), (579, 301), (706, 325), (703, 242), (578, 218), (476, 181)],
     ],
     'centros_zonas_estacionados': [(75, 195), (480, 185)],
-    'mostrar_velocidades': False
+    'mostrar_trayectorias': True,
+    'mostrar_velocidades': False,
+    'velocidades_por_zonas': False,
+    'zonas_velocidades': [
+        [(316, 480), (232, 428), (563, 199), (720, 208), (720, 480)],
+        [(232, 428), (563, 199), (457, 160), (74, 300)],
+        [(74, 300), (457, 160), (352, 87), (0, 195)],
+    ],
+    'min_max_zonas': [
+        [0, 100],
+        [0, 90],
+        [0, 70],
+    ],
+    'guardar_videos_evento': True,
 }
 
 def main(rstp_url, model, clases, fps, procesamiento):
     cap = get_video(rstp_url)
     model = get_model(model)
+    #print(model.model.names)
     run_detect(cap, model, clases, fps, procesamiento)
 
+
+
 if __name__ == '__main__':
+
+    #p1 = threading.Thread(target=Receive)
+    #p2 = threading.Thread(target=Display)
+    #p1 = multiprocessing.Process(target=Receive)
+    #p2 = multiprocessing.Process(target=Display)
+    #p1.start()
+    #p2.start()
     main(rtsp_url, MODEL, selected_classes, fps, procesamiento)
+
+
+def Receive():
+    cap = get_video(rtsp_url)
+    ret, frame = cap.read()
+    q.put(frame)
+    while ret:
+        ret, frame = cap.read()
+        #print('a')
+        q.put(frame)
+        #print('b')
+
+def Display():
+    model = get_model(MODEL)
+    i = 0
+    tiempos = []
+    clases = selected_classes
+
+    if not procesamiento['solo_mostrar']:
+        tareas = Objeto_Global(fps)
+        tareas.create_byte_tracker()
+        if procesamiento['solo_detector']:
+            tareas.create_detector()
+        if procesamiento['mostrar_estacionados']:
+            tareas.create_estacionados(frame, procesamiento['zonas_estacionados'], procesamiento['centros_zonas_estacionados'])
+        if procesamiento['mostrar_contadores']:
+            tareas.create_contadores(procesamiento['lineas_contadores'])
+        if procesamiento['mostrar_trayectorias']:
+            tareas.create_trayectorias()
+        if procesamiento['mostrar_velocidades']:
+            tareas.create_velocidades()
+
+    while True:
+        if q.empty() != True:
+            frame = q.get()
+            ti = time.time()
+            if procesamiento['solo_mostrar']:
+                output_frame = frame
+            else:
+                #print('c')
+                output_frame = detect(frame, model, clases, tareas)
+                #print('d')
+                tiempos.append((time.time() - ti)*1000)
+                i += 1
+            cv2.imshow('Camara Paseo Estacion con analitica', output_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
