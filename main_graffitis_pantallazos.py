0b53841c75c3a2181476c1ff8a7b3cfa641885c6
Eitanaso <53804725+Eitanaso@users.noreply.github.com>
2024-05-16 17:01:21 -0400

    Add files via upload

--- main_graffitis_pantallazos.py
+++ main_graffitis_pantallazos.py
@@ -29,6 +29,8 @@ from datetime import date, datetime
 import pytz
 import boto3
 
+import re
+
 #SOURCE_VIDEO_PATH = 'c:\\Users\\Analitica2\\Desktop\\test\\comercio_ambulante_paseo_estacion.mp4'
 #rtsp_url = SOURCE_VIDEO_PATH
 MODEL = "yolov8_detector_graffitis.pt"
@@ -37,6 +39,7 @@ selected_classes = 0
 segs_frame = 5
 #fps = 25
 
+global user_inputs
 user_inputs = []
 
 
@@ -51,7 +54,9 @@ bucket = 'camare-iot-120474950462'
 tz = pytz.timezone('America/Santiago') #America/Mexico_City , #America/Bogota
 
 CLIENT_NAME = "sensor-pruebas"
-TOPIC = "iot-sensor/device-mx/001"
+global TOPIC
+#TOPIC = "iot-sensor/device-mx/001" # 001 -> IDcam-subID-comuna-calle1-calle2 (quitar caracteres especiales)
+#TOPIC = "iot-sensor/device-mx/"
 PHAT = "D:\\analitica_camara_CEGIR\\cer\\"
 
 BROKER_PATH = "a1htxdpw7uo3bd-ats.iot.us-east-1.amazonaws.com"
@@ -76,7 +81,7 @@ IoTclient.configureConnectDisconnectTimeout(10)
 IoTclient.configureMQTTOperationTimeout(5) 
 
 IoTclient.connect()
-IoTclient.publish(TOPIC, "connected", 0)
+#IoTclient.publish(TOPIC, "connected", 0)
 
 # Create and Send Payloads to the IoT Topic
 def create_payload(datos):
@@ -92,52 +97,98 @@ def create_payload(datos):
 	return payload
 
 def get_user_input():
-    user_input = entry_ID.get()
-    user_inputs.append(user_input)
-    user_input = entry_subID.get()
-    user_inputs.append(user_input)
-    user_input = entry_calle1.get()
-    user_inputs.append(user_input)
-    user_input = entry_calle2.get()
-    user_inputs.append(user_input)
-    user_input = entry_comuna.get()
-    user_inputs.append(user_input)
+    info_obligatoria = True
+    #print('2', user_inputs)
+    #user_inputs = []
+    #print('2', user_inputs)
+
+    global user_inputs
+
+    if entry_ID.get():
+        user_input = entry_ID.get()
+        user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+    else:
+        info_obligatoria = False
+        print('Falta ID de Camara')
+        entry_ID.focus_set()
+
+    if entry_subID.get():
+        user_input = entry_subID.get()
+        user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+    else:
+        info_obligatoria = False
+        print('Falta sub ID de Camara')
+        entry_ID.focus_set()
+
+    if entry_comuna.get():
+        user_input = entry_comuna.get()
+        user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+    else:
+        info_obligatoria = False
+        print('Falta comuna de Camara')
+        entry_ID.focus_set()
+
+    if entry_calle1.get():
+        user_input = entry_calle1.get()
+        user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+    else:
+        info_obligatoria = False
+        print('Falta calle 1 de Camara')
+        entry_ID.focus_set()
+
+    if entry_calle2.get():
+        user_input = entry_calle2.get()
+        user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+    else:
+        info_obligatoria = False
+        print('Falta calle 2 de Camara')
+        entry_ID.focus_set()
+
     user_input = entry_enfoque.get()
-    user_inputs.append(user_input)
+    user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+
     user_input = entry_marca.get()
-    user_inputs.append(user_input)
+    user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+
     user_input = entry_modelo.get()
-    user_inputs.append(user_input)
-    window.destroy()
+    user_inputs.append(re.sub('[^A-Za-z0-9]+', '', user_input))
+
+    #print('3', user_inputs)
+
+    if info_obligatoria:
+        window.destroy()
+    else:
+        user_inputs = []
+        print('-------------------------------------------------------------')
 
 window = tk.Tk()
 label = tk.Label(window, text='Datos utiles')
 label.pack()
 
-label = tk.Label(window, text='ID camara:')
+label = tk.Label(window, text='ID camara (obligatorio):') #obligatorio
 label.pack()
 entry_ID = tk.Entry(window)
 entry_ID.pack()
 
-label = tk.Label(window, text='Sub ID camara:')
+label = tk.Label(window, text='Sub ID camara (obligatorio):') #obligatorio
 label.pack()
 entry_subID = tk.Entry(window)
 entry_subID.pack()
 
-label = tk.Label(window, text='Nombre calle 1:')
+label = tk.Label(window, text='Comuna (obligatorio):') #obligatorio
+label.pack()
+entry_comuna = tk.Entry(window)
+entry_comuna.pack()
+
+label = tk.Label(window, text='Nombre calle 1 (obligatorio):') #obligatorio
 label.pack()
 entry_calle1 = tk.Entry(window)
 entry_calle1.pack()
 
-label = tk.Label(window, text='Nombre calle 2:')
+label = tk.Label(window, text='Nombre calle 2 (obligatorio):') #obligatorio
 label.pack()
 entry_calle2 = tk.Entry(window)
 entry_calle2.pack()
-
-label = tk.Label(window, text='Comuna:')
-label.pack()
-entry_comuna = tk.Entry(window)
-entry_comuna.pack()
 
 label = tk.Label(window, text='Direccion cardinal que enfoca la camara:')
 label.pack()
@@ -188,10 +239,10 @@ def main(model, i=0):
     img = ImageGrab.grab()
     img = np.asarray(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.imshow('frame', img)
+    cv2.imshow('Seleccion de 2 puntos de camara', img)
     global puntos
     puntos = []
-    cv2.setMouseCallback('frame', click_event)
+    cv2.setMouseCallback('Seleccion de 2 puntos de camara', click_event)
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -200,15 +251,25 @@ def main(model, i=0):
     bbox = (puntos[0][0], puntos[0][1],puntos[1][0],puntos[1][1])
     #while cap.isOpened():
     df=pd.DataFrame({'Hora Deteccion':[], 'Cantidad Detectado':[]})
+
+    #print('1', user_inputs)
     
     window.mainloop()
+
+    #global TOPIC
+
+    TOPIC = "iot-sensor/device-mx/" + f'{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}' #IDcam-subID-comuna-calle1-calle2
+
+    IoTclient.publish(TOPIC, "connected", 0)
+
+    #print('4', user_inputs)
 
     frame = ImageGrab.grab(bbox=bbox)
     frame = np.asarray(frame)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    cv2.imshow('frame', frame)
+    cv2.imshow('Seleccion de zona de deteccion', frame)
     puntos = []
-    cv2.setMouseCallback('frame', click_event)
+    cv2.setMouseCallback('Seleccion de zona de deteccion', click_event)
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -230,29 +291,30 @@ def main(model, i=0):
         #print(output_frame.shape)
         #print(user_inputs)
         #print(len(dets))
-        cv2.imshow('Camara Paseo Estacion con analitica', output_frame)
+        cv2.imshow('Detecciones', output_frame)
         if (prev_det == 0) and (prev_det < len(dets)):
-            momento = datetime.now().strftime("%d_%m_%Y %H_%M_%S")
+            momento = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
             datetime_local = datetime.now(tz)
-            cv2.imwrite(f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}.jpg', output_frame)
+            cv2.imwrite(f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}_{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}.jpg', output_frame)
             datos = {
-			"type": "GS",
-            'id_camara': user_inputs[0], 'sub_id_camara': user_inputs[1], 'nombre_calle1': user_inputs[2], 'nombre_calle2': user_inputs[3],
-                     'comuna': user_inputs[4], 'direccion_cardinal_enfoque_camara': user_inputs[5], 'marca_camara': user_inputs[6],
+			"type": "GS", "id": str(uuid.uuid4()), 'region': 'Metropolitana de Santiago', 'Provincia': 'Santiago',
+            'id_camara': user_inputs[0], 'sub_id_camara': user_inputs[1], 'comuna': user_inputs[2], 
+            'nombre_calle1': user_inputs[3], 'nombre_calle2': user_inputs[4],
+                     'direccion_cardinal_enfoque_camara': user_inputs[5], 'marca_camara': user_inputs[6],
                      'modelo': user_inputs[7], 'resolucion_screenshot': output_frame.shape,
                 #'dia': momento.split(' ')[0], 'hora_min_seg': momento.split(' ')[1], 
 			    "date": datetime_local.strftime("%Y-%m-%d %H:%M:%S"),
 			    "timestamp": int(time.time()),
                 'modelo_detector': model_name,
                 'graffiti_detectado': 1, 'cantidad_detectada': len(dets),
-                'nombre_imagen': f'{momento}.jpg',
+                'nombre_imagen': f'{momento}_{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}.jpg', #quitar caracteres especiales
                 }
-            #IoTclient.publish(TOPIC, create_payload(datos), 0)
-            with open(f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}.json', 'w') as f:
+            IoTclient.publish(TOPIC, create_payload(datos), 0)
+            with open(f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}_{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}.json', 'w') as f:
                 json.dump(datos, f)
-            file_name = f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}.jpg'
-            key_name = f'{momento}.jpg'
-            #s3.upload_file(file_name, bucket, key_name)
+            file_name = f'D:\\analitica_camara_CEGIR\\datos_graffiti\\{momento}_{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}.jpg'
+            key_name = f'{momento}_{user_inputs[0]}_{user_inputs[1]}_{user_inputs[2]}_{user_inputs[3]}_{user_inputs[4]}.jpg'
+            s3.upload_file(file_name, bucket, key_name)
             #print('Nuevo graffiti')
         prev_det = len(dets)
         now = datetime.now()
